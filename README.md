dfd5ddf2501abd1d5259d2c87795725bded0f066
anis <shahidsarker95@hotmail.com>
2021-09-15 01:29:46 +0600

    argparse done

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ author: Anis Sarker
 
 # Flac metadata extractor
 
-This program is to extract metadata of a flac audio file.
+This program is use to extract metadata of a flac file.
 
 * Load an audio flac file
 * Read the metadata
@@ -20,76 +20,23 @@ This program is to extract metadata of a flac audio file.
 
     `$ pip install -U audio-metadata`
 
+## Run
+
+`$ python music.py --audio '/path/to/files' --output '/path/to/output' --file some_name`
+
+
 ## Usage
-Copy the code and save as any name such as music.py
 
-Create a directory inside project folder and name it *dist* to keep the flac files.
+```
+music.py [-h] [-a AUDIO] [-o OUTPUT] [-f FILE]
 
-**Run**
+Flac audio metadata extractor
 
-`$ python music.py`
-
-**Output**
-
-album-arts will be saved in directiry `dist/album-art`.
-
-a `music.json` will be saved inside `dist` diretory.
-
-### **Code**
-
-```python
-import os
-import audio_metadata
-import json
-
-
-music_dir = 'dist/music'
-album_art_dir = 'dist/album-art'
-music_info = []
-id = 0
-
-if os.path.exists(album_art_dir):
-    print("+---------------------------------------------------+")
-    print("| dist/album-art directory already exists. skipping |")
-    print("+---------------------------------------------------+")
-    pass
-else:
-    os.makedirs(album_art_dir)
-    print("+---------------------------------------------------+")
-    print("| dist/album-art directory created...               |")
-    print("+---------------------------------------------------+")
-
-for music in os.listdir(music_dir):
-    print(f"current image\nid: {id}\nfilename: {music}\n{len(music)*'-'}----------\n")
-
-    file_ = os.path.join(music_dir, music)
-    meta_data = audio_metadata.load(file_)
-
-    img = meta_data["pictures"]
-    for pic in img:
-        if pic.type == 3:
-            album_art_path = os.path.join(album_art_dir, f"{str(id)}.jpg")
-            with open(album_art_path, 'wb') as f:
-                f.write(pic.data)
-
-    music_info.append(
-        {
-            "id": str(id),
-            "title": meta_data["tags"]["title"][0],
-            "album": meta_data["tags"]["album"][0],
-            "artist": meta_data["tags"]["artist"][0],
-            "genre": meta_data["tags"]["genre"][0],
-            "date": meta_data["tags"]["date"][0],
-            "duration": meta_data["tags"]["album"][0],
-            "filepath": meta_data["filepath"],
-            "filesize": meta_data["filesize"],
-            "albumart": album_art_path
-        }
-    )
-    id += 1
-
-with open('dist/music.json', "w") as outFile:
-    json.dump(music_info, outFile)
-    print("file saved as json...\n")
-    outFile.close()
+optional arguments:
+  -h, --help            show this help message and exit
+  -a AUDIO, --audio AUDIO
+                        Path of flac file
+  -o OUTPUT, --output OUTPUT
+                        Output directory
+  -f FILE, --file FILE  JSON output name
 ```
--- music.py
+++ music.py
@@ -1,54 +1,59 @@
 import os
+import glob
+import argparse
 import audio_metadata
 import json
 
+def extractor(audio, output, file):
+    album_art_dir = '{}/album-art'.format(output)
+    music_info = []
+    id = 0
 
-music_dir = 'dist/music'
-album_art_dir = 'dist/album-art'
-music_info = []
-id = 0
+    if os.path.exists(album_art_dir):
+        pass
+    else:
+        os.makedirs(album_art_dir)
+    for music in glob.glob("{}/*.flac".format(audio)):
+        tmp_music = { "id": "", "title": "", "album": "", "artist": "", "genre": "", "date": "", "filepath": "", "filesize": "", "albumart": ""}
+        file_ = os.path.join(audio, music)
+        meta_data = audio_metadata.load(file_)
+        img = meta_data["pictures"]
+        for pic in img:
+            if pic.type == 3:
+                album_art_path = os.path.join(album_art_dir, f"{str(id)}.jpg")
+                with open(album_art_path, 'wb') as f:
+                    f.write(pic.data)
 
-if os.path.exists(album_art_dir):
-    print("+---------------------------------------------------+")
-    print("| dist/album-art directory already exists. skipping |")
-    print("+---------------------------------------------------+")
-    pass
-else:
-    os.makedirs(album_art_dir)
-    print("+---------------------------------------------------+")
-    print("| dist/album-art directory created...               |")
-    print("+---------------------------------------------------+")
+        tmp_music["id"] = str(id)
+        tmp_music["title"] = meta_data["tags"]["title"][0] if meta_data["tags"].get("title") else "Track {}".format(id)
+        tmp_music["album"] = meta_data["tags"]["album"][0] if meta_data["tags"].get("album") else "Unknown"
+        tmp_music["artist"] = meta_data["tags"]["artist"][0] if meta_data["tags"].get("artist") else "Unknown"
+        tmp_music["genre"] = meta_data["tags"]["genre"][0] if meta_data["tags"].get("genre") else "Unknown"
+        tmp_music["date"] = meta_data["tags"]["date"][0] if meta_data["tags"].get("date") else "Unknown"
+        tmp_music["filepath"] = meta_data["filepath"] if meta_data.get("filepath") else "Unknown"
+        tmp_music["filesize"] = meta_data["filesize"] if meta_data.get("filesize") else "Unknown"
+        try: 
+            tmp_music["albumart"] = album_art_path
+        except KeyError:
+            tmp_music["albumart"] = "Unknown"
 
-for music in os.listdir(music_dir):
-    print(f"current image\nid: {id}\nfilename: {music}\n{len(music)*'-'}----------\n")
+        music_info.append(tmp_music)
+        id += 1
 
-    file_ = os.path.join(music_dir, music)
-    meta_data = audio_metadata.load(file_)
+    print(music_info)
+    with open(f'{output}/{file}.json', "w") as outFile:
+        json.dump(music_info, outFile)
+        print("file saved as {}.json...".format(file))
+        outFile.close()
+
+def main():
+    parser = argparse.ArgumentParser(description="Flac audio metadata extractor")
+    parser.add_argument('-a', '--audio', type=str, help="Path of flac file")
+    parser.add_argument('-o', '--output', type=str, help="Output directory")
+    parser.add_argument('-f', '--file', type=str, help="JSON output name")
+    args = parser.parse_args()
     
-    img = meta_data["pictures"]
-    for pic in img:
-        if pic.type == 3:
-            album_art_path = os.path.join(album_art_dir, f"{str(id)}.jpg")
-            with open(album_art_path, 'wb') as f:
-                f.write(pic.data)
+    extractor(args.audio, args.output, args.file)
 
-    music_info.append(
-        {
-            "id": str(id),
-            "title": meta_data["tags"]["title"][0],
-            "album": meta_data["tags"]["album"][0],
-            "artist": meta_data["tags"]["artist"][0],
-            "genre": meta_data["tags"]["genre"][0],
-            "date": meta_data["tags"]["date"][0],
-            "duration": meta_data["tags"]["album"][0],
-            "filepath": meta_data["filepath"],
-            "filesize": meta_data["filesize"],
-            "albumart": album_art_path
-        }
-    )
-    id += 1
-
-with open('dist/music.json', "w") as outFile:
-    json.dump(music_info, outFile)
-    print("file saved as json...\n")
-    outFile.close()
+if __name__ == "__main__":
+    main()
